990c00f4a4815424fc809e16bca5da3437ac8906
dgliyu <lhcxndx@126.com>
2026-01-21 18:37:20 +0800

    test async and promise, and unique_lock can used with condition_variable

--- async_thread.cpp
+++ async_thread.cpp
@@ -14,7 +14,9 @@ string TestAsync(int index)
 int main(int argc, char* argv[])
 {
    //创建异步线程  23async创建异步线程替代thread
-    
+   
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test1: use TestFuture in thread" << std::endl; 
     cout << "main thread id " << this_thread::get_id() << endl;
     //不创建线程启动异步任务
     auto future = async(launch::deferred, TestAsync,100);
@@ -30,6 +32,49 @@ int main(int argc, char* argv[])
     cout << "begin future2 get " << endl;
     cout << "future2.get() = " << future2.get() << endl;
     cout << "end future2 get" << endl;
+
+
+
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test2: catch exception through future and async" << std::endl;
+    // 如果崩溃发生在thread内，而未被catch, 则会导致主线程terminate
+    auto testException = []()
+    {
+        throw std::runtime_error("crash in lambda");
+    };
+    auto fut2 = std::async(std::launch::async, testException);
+    try
+    {
+        // 子线程里抛出的异常不会丢。它会被 future 收起来。
+        // 等你 get() 的时候再抛回给你。这一下就很像同步函数了。
+        fut2.get();
+        // wait() 很像你在问：活做完没？
+        // get() 则是：活做完没？给我成果。
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test3:  sharefuture and async" << std::endl;
+    auto fut3 = std::async(std::launch::async, [](){return 45;});
+    // 通过future获取shared_future
+    std::shared_future<int> sf3 = fut3.share();
+    int res1 = sf3.get();
+    int res2 = sf3.get();
+    // shared_future 的直觉: 普通 future 像一份一次性的快递。shared_future 像一份复印件。你可以拿很多次。
+    std::cout << "shared_future res1 = " << res1 << std::endl;
+    std::cout << "shared_future res2 = " << res2 << std::endl;
+
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test4: deferred async" << std::endl;
+    auto func = std::async(std::launch::deferred, [](){return 45;});
+    func.get();
+    // 函数不是立刻跑的。而是你 get() 的那一刻。才在当前线程开始跑。
+
 
 
     getchar();
--- promise_future.cpp
+++ promise_future.cpp
@@ -23,14 +23,91 @@ int main(int argc, char* argv[])
     //用来获取线程异步值获取
     auto future = p.get_future();
 
-    auto th = thread(TestFuture, move(p));
+    auto th = thread(TestFuture, std::move(p));
 
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test1: use TestFuture in thread" << std::endl;
     cout << "begin future.get()" << endl;
     cout <<  "future get() = " << future.get() << endl;
     cout << "end future.get()" << endl;
     th.join();
 
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test2: use thread without async" << std::endl;
+    // 不使用async还是会把值返回回去
+    std::promise<int> pInt;
+    std::future<int> pFuture = pInt.get_future();
 
+    // 在 lambda 里面新建了一个变量 pm。把外面的 pInt move 进来。然后线程里用的其实是 pm。
+    // mutable 的意思是：允许这个 lambda 里修改 pm。
+    std::thread ppThread([pm = std::move(pInt)]() mutable{
+        pm.set_value(45); 
+    });
+    ppThread.join();
+    int val = pFuture.get();
+    std::cout << "Test2: use thread without async and future get: " << val << std::endl;
+
+
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test3: collect exception through promise" << std::endl;
+    std::promise<int> pt3;
+    std::future<int> pt3Future = pt3.get_future();
+    std::thread pt3Thread([pm3 = std::move(pt3)]() mutable
+    {
+        try
+        {
+            throw 1;
+        }
+        catch(...) //const std::exception& e
+        {
+            // 没有mutable不能修改promise的值
+            pm3.set_exception(std::current_exception());
+            // std::cerr << e.what() << '\n';
+        }
+    });
+    pt3Thread.join();
+
+    try
+    {
+        pt3Future.get();
+    }
+    catch (int e)
+    {
+        std::cout << "Captured int exception: " << e << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Captured std::exception: " << e.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Captured unknown exception" << '\n';
+    }
+    
+
+    std::cout << "==================================" << std::endl;
+    std::cout << "Test4: collect exception through promise without assign val" << std::endl;
+    std::future<int> pt4Future;    
+    {
+        std::promise<int> pt4;
+        pt4Future = pt4.get_future();
+    }
+    try
+    {
+        pt4Future.get();
+    }
+    catch (int e)
+    {
+        std::cout << "Captured int exception: " << e << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Captured std::exception: " << e.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Captured unknown exception" << '\n';
+    }
 
     getchar();
     return 0;
